67418506695bce2bef54a324c9e50ddfd8f883c3
Tom Winter <tom.winter@posteo.net>
2023-04-27 19:02:52 +0200

    use env vars to set DB config

--- src/configuration/sequelize.configuration.js
+++ src/configuration/sequelize.configuration.js
@@ -1,8 +1,14 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize("lunchbot","lunch","bot",{
-    host: "localhost",dialect: 'postgres', logging: false, benchmark: true
-});
+const sequelize = new Sequelize(
+    process.env["DB_TABLE"] || "lunchbot",
+    process.env["DB_USER"] || "lunch",
+    process.env["DB_PASSWORD"] || "bot", {
+        host: process.env["DB_HOST"] || "localhost",
+        dialect: process.env["DB_DIALECT"] || 'postgres',
+        logging: false,
+        benchmark: true
+    });
 
 const modelDefiners = [require("../model/food.model"), require("../model/vote.model")]
 
