844b326027261b28c1f21d9ac199e6b9a10fa28f
Tom Winter <tom.winter@posteo.net>
2023-04-27 20:31:20 +0200

    reordered endpoints in test router

--- src/router/test.router.js
+++ src/router/test.router.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const lunchService = require('../service/lunch.service')
 
+router.get('/vote', async function (req, res) {
+    const votes = await lunchService.getVotes()
+    res.json([...votes])
+})
+
 router.post('/vote', async function (req, res) {
     const user = req.query.user
     const food = req.query.food
     await lunchService.voteLunch(user, [food])
     res.json({user: user, food: food});
-});
-
-router.get('/vote', async function (req, res) {
-    const votes = await lunchService.getVotes()
-    res.json([...votes])
 })
 
 router.get('/food', async (req, res) => {
