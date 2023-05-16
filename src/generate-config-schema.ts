129a58226256b39022831c5d9db2da28f0dd8238
Tom Winter <tom.winter@posteo.net>
2023-05-16 23:15:46 +0200

    added generation of a json schema for the config file

---
+++ src/generate-config-schema.ts
@@ -0,0 +1,13 @@
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { configurationSchema } from "./configuration/configuration.schema";
+import * as fs from "fs";
+
+const schemaJson = zodToJsonSchema(
+    configurationSchema,
+    "LunchBot configuration schema"
+);
+
+const schemaStr = JSON.stringify(schemaJson, null, 4);
+fs.writeFileSync("./configuration-schema.json", schemaStr);
+
+console.log("Schema successfully updated.");
